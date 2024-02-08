64ff44a4a3bea57a309da6b3f286c6c754cbb2b6
Ednardo Gomes <ednardo_10.dev@hotmail.com>
2024-02-08 17:19:26 -0300

    refactor: convert component to Class

--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -1,9 +1,25 @@
-const TaskItem = ({ task }) => {
-  return (
-    <>
-      <h1>{task.description}</h1>
-      <p>{task.isCompleted ? "Completa" : "Não completa"}</p>
-    </>
-  );
-};
+// const TaskItem = ({ task }) => {
+//   return (
+//     <>
+//       <h1>{task.description}</h1>
+//       <p>{task.isCompleted ? "Completa" : "Não completa"}</p>
+//     </>
+//   );
+// };
+// export default TaskItem;
+
+//Método abaixo é o método componentes de Classe
+import React from "react";
+class TaskItem extends React.Component {
+  render() {
+    const { task } = this.props;
+    return (
+      <>
+        <h1>{task.description}</h1>
+        <p>{task.isCompleted ? "Completa" : "Não completa"}</p>
+      </>
+    );
+  }
+}
+
 export default TaskItem;
