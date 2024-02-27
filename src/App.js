4792d4670622055faa3393dba4d242d00a27c09d
Ednardo Gomes <ednardo_10.dev@hotmail.com>
2024-02-27 17:09:56 -0300

    style: add sass and change font family

--- src/App.js
+++ src/App.js
@@ -1,75 +1,90 @@
 //Componente de classes
-import React from "react";
-import TaskItem from "./components/TaskItem";
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleStateChange = this.handleStateChange.bind(this);
-    this.state = {
-      tasks: [
-        {
-          id: "1",
-          description: "Estudar Programação",
-          isCompleted: false,
-        },
-        {
-          id: "2",
-          description: "Ler",
-          isCompleted: true,
-        },
-      ],
-    };
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    console.log(prevState);
-    console.log("component wasd update!");
-  }
-
-  handleStateChange() {
-    this.setState({
-      tasks: [],
-    });
-  }
-
-  render() {
-    return (
-      <>
-        {this.state.tasks.map((task) => (
-          <TaskItem key={task.id} task={task} />
-        ))}
-        <button onClick={this.handleStateChange}>Limpar tarefas</button>
-      </>
-    );
-  }
-}
-
-export default App;
-
-// import { useState } from "react";
+// import React from "react";
 // import TaskItem from "./components/TaskItem";
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.handleStateChange = this.handleStateChange.bind(this);
+//     this.state = {
+//       tasks: [
+//         {
+//           id: "1",
+//           description: "Estudar Programação",
+//           isCompleted: false,
+//         },
+//         {
+//           id: "2",
+//           description: "Ler",
+//           isCompleted: true,
+//         },
+//       ],
+//     };
+//   }
 
-// const App = () => {
-//   const [tasks, setTasks] = useState([
-// {
-//   id: "1",
-//   description: "Estudar Programação",
-//   isCompleted: false,
-// },
-// {
-//   id: "2",
-//   description: "Ler",
-//   isCompleted: true,
-// },
-//   ]);
+//   componentDidUpdate(prevProps, prevState) {
+//     console.log(prevState);
+//     console.log("component wasd update!");
+//   }
 
-//   return (
-// <>
-//   {tasks.map((task) => (
-//     <TaskItem key={task.id} task={task} />
-//   ))}
-// </>
-//   );
-// };
+//   handleStateChange() {
+//     this.setState({
+//       tasks: [],
+//     });
+//   }
+
+//   render() {
+//     return (
+//       <>
+//         {this.state.tasks.map((task) => (
+//           <TaskItem key={task.id} task={task} />
+//         ))}
+//         <button onClick={this.handleStateChange}>Limpar tarefas</button>
+//       </>
+//     );
+//   }
+// }
 
 // export default App;
+
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+import TaskItem from "./components/TaskItem";
+
+const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: "1",
+      description: "Estudar Programação",
+      isCompleted: false,
+    },
+    {
+      id: "2",
+      description: "Ler",
+      isCompleted: true,
+    },
+  ]);
+
+  const fetchTasks = async () => {
+    try {
+      const { data } = await axios.get("localhost:3000/tasks");
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTasks();
+  });
+
+  return (
+    <>
+      {tasks.map((task) => (
+        <TaskItem key={task.id} task={task} />
+      ))}
+    </>
+  );
+};
+
+export default App;
--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -1,32 +1,35 @@
-// const TaskItem = ({ task }) => {
-//   return (
-//     <>
-//       <h1>{task.description}</h1>
-//       <p>{task.isCompleted ? "Completa" : "Não completa"}</p>
-//     </>
-//   );
-// };
-// export default TaskItem;
+import { useEffect } from "react";
+const TaskItem = ({ task }) => {
+  useEffect(() => {
+  }, []);
+  return (
+    <>
+      <h1>{task.description}</h1>
+      <p>{task.isCompleted ? "Completa" : "Não completa"}</p>
+    </>
+  );
+};
+export default TaskItem;
 
 //Método abaixo é o método componentes de Classe
-import React from "react";
-class TaskItem extends React.Component {
-  componentDidMount() {
-    console.log("component was mounted");
-  }
+// import React from "react";
+// class TaskItem extends React.Component {
+//   componentDidMount() {
+//     console.log("component was mounted");
+//   }
 
-  componentWillUnmount() {
-    console.log("i will unmount!");
-  }
-  render() {
-    const { task } = this.props;
-    return (
-      <>
-        <h1>{task.description}</h1>
-        <p>{task.isCompleted ? "Completa" : "Não completa"}</p>
-      </>
-    );
-  }
-}
+//   componentWillUnmount() {
+//     console.log("i will unmount!");
+//   }
+//   render() {
+//     const { task } = this.props;
+//     return (
+//       <>
+//         <h1>{task.description}</h1>
+//         <p>{task.isCompleted ? "Completa" : "Não completa"}</p>
+//       </>
+//     );
+//   }
+// }
 
-export default TaskItem;
+// export default TaskItem;
